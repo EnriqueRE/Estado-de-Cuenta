6f9bd1b065e9d29e9c8ea6dbbe8d022b14120544
Enrique Ramirez <enrique.ramirez35@gmail.com>
2014-08-07 11:04:03 -0600

    Fixed Serializers error
    nt

--- Estado/serializers.py
+++ Estado/serializers.py
@@ -3,15 +3,18 @@ from rest_framework import serializers
 from models import MonederoUser, AccountStatement
 
 
-class AccountSerializer (serializers.ModelSerializer):
-     class Meta:
-          model = AccountStatement
-          fields = ('statement_id', 'statement_student', 'statement_date', 'statement_tuition','statement_interests',
-                    'statement_insurance', 'statement_diverse_services')
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccountStatement
+        fields = ('statement_id', 'statement_student', 'statement_date',
+                  'statement_tuition', 'statement_interests',
+                  'statement_positive_balance',
+                  'statement_insurance', 'statement_diverse_services')
 
 
-class UserSerializer (serializers.ModelSerializer):
-     class Meta:
-          model = MonederoUser
-          fields = ('student_id', 'student_name', 'student_middle_name', 'student_lastname',
-                    'student_second_lastname', 'student_badge_id', 'student_email')
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MonederoUser
+        fields = (
+        'student_id', 'student_name', 'student_middle_name', 'student_lastname',
+        'student_second_lastname', 'student_badge_id', 'student_email')
